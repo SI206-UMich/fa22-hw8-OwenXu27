944832eaae90506c85ab9f514f4ee4f155e5ca78
Owen Xu <owenxu@Owens-MacBook-Pro-5.local>
2022-12-09 11:13:46 -0500

    1st

--- HW8.py
+++ HW8.py
@@ -9,7 +9,21 @@ def get_restaurant_data(db_filename):
     dictionaries. The key:value pairs should be the name, category, building, and rating
     of each restaurant in the database.
     """
-    pass
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_filename)
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT r.name,c.category,b.building, r.rating FROM restaurants r, categories c, buildings b WHERE r.category_id = c.id AND r.building_id = b.id"
+    )
+    info_list = []
+    for i in cur.fetchall():
+        dict = {}
+        dict['name'] = i[0]
+        dict['category'] = i[1]
+        dict['building'] = i[2]
+        dict['rating'] = i[3]
+        info_list.append(dict)
+    return info_list
 
 def barchart_restaurant_categories(db_filename):
     """
@@ -17,7 +31,27 @@ def barchart_restaurant_categories(db_filename):
     restaurant categories and the values should be the number of restaurants in each category. The function should
     also create a bar chart with restaurant categories and the counts of each category.
     """
-    pass
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_filename)
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT c.category,COUNT(*) FROM restaurants r, categories c WHERE r.category_id = c.id GROUP BY c.category"
+    )
+    dict = {}
+    for i in cur.fetchall():
+        dict[i[0]] = dict.get(i, 0) + i[1]
+
+    categories = list(dict.keys())
+    val = list(dict.values())
+ 
+    plt.barh(categories, val)
+    
+    plt.ylabel("Restaurant Categories")
+    plt.xlabel("No. of Restaurants")
+    plt.title("Types of Restaurants in South University")
+    plt.tight_layout()
+    plt.show()
+    return dict
 
 #EXTRA CREDIT
 def highest_rated_category(db_filename):#Do this through DB as well
@@ -27,11 +61,41 @@ def highest_rated_category(db_filename):#Do this through DB as well
     in that category. This function should also create a bar chart that displays the categories along the y-axis
     and their ratings along the x-axis in descending order (by rating).
     """
-    pass
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_filename)
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT category, rating
+        FROM restaurants 
+        JOIN categories 
+        ON restaurants.category_id = categories.id 
+        GROUP BY category_id
+        ORDER BY rating ASC
+        """
+    )
+    data = cur.fetchall()
+    new = {}
+    for i in data:
+        new[i[0]] = new.get(i, 0) + i[1]
+
+    categories = list(new.keys())
+    val = list(new.values())
+
+    plt.barh(categories, val)
+    
+    plt.ylabel("Categories")
+    plt.xlabel("Ratings")
+    plt.title("Average Restaurant Ratings by Category")
+    plt.tight_layout()
+    plt.show()
+    return data[-1]
 
 #Try calling your functions here
 def main():
-    pass
+    first = get_restaurant_data('South_U_Restaurants.db')
+    second = barchart_restaurant_categories('South_U_Restaurants.db')
+    third = highest_rated_category('South_U_Restaurants.db')
 
 class TestHW8(unittest.TestCase):
     def setUp(self):
